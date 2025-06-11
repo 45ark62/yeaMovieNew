79f34b253bcbe6540e8c4455eb7af48755fbb9fa
Эльвира <45ark97@gmail.com>
2025-06-11 11:00:40 +0300

    feat: :sparkles: Добавляет страницу 404

--- src/pages/not-found/ui/NotFound.tsx
+++ src/pages/not-found/ui/NotFound.tsx
@@ -1,8 +1,16 @@
-
+import { Link } from 'react-router-dom'
+import styles from '../styles.module.css'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+     <div className={styles.container}>
+      <h1 className={styles.code}>404</h1>
+      <h2 className={styles.title}>Страница не найдена</h2>
+      <p className={styles.text}>Упс! Похоже, вы перешли по несуществующему адресу.</p>
+      <Link to="/" className={styles.link}>
+        Вернуться на главную
+      </Link>
+    </div>
   )
 }
 
